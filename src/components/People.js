23e6163b71f2c16830f3f6df43cdb81cc232cd41
Esteban Herrera Vargas <nabetse.hv.09@gmail.com>
2022-05-07 02:51:29 -0600

    FEATURE: Changes in the default react url and default.conf to include other apis

--- src/components/People.js
+++ src/components/People.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Segment, Card, Button } from "semantic-ui-react";
+import { Segment, Card, Button, CardGroup } from "semantic-ui-react";
 import axios from "axios";
 
 export default class People extends Component {
@@ -26,40 +26,49 @@ export default class People extends Component {
   };
 
   PeopleCards = () => {
-    return (
-      <Card.Group>
-        {
-        this.state.people.map((person) => {
-          return (
-            <Card key={person._id}>
-              <Card.Content>
-                <Card.Header>{person.name}</Card.Header>
-                <Card.Meta>Age: {person.age}</Card.Meta>
-                <Card.Meta>Background: {person.background}</Card.Meta>
-                <Card.Description>{person.description}</Card.Description>
-              </Card.Content>
-              <Card.Content extra style={{ textAlign: "center" }}>
-                <Button
-                  color="red"
-                  onClick={() => {
-                    axios
-                      // .delete("http://localhost/api/v1" + "/person/" + person._id)
-                      .delete("/api/v1/person/" + person._id)
-                      .then((res) => {
-                        console.log(res);
-                        this.fetchPeople();
-                      })
-                      .catch((err) => console.log(err));
-                  }}
-                >
-                  Delete
-                </Button>
-              </Card.Content>
-            </Card>
-          );
-        })}
-      </Card.Group>
-    );
+    if (this.state.people.length > 0){
+      return (
+        <Card.Group>
+          {
+          this.state.people.map((person) => {
+            return (
+              <Card key={person._id}>
+                <Card.Content>
+                  <Card.Header>{person.name}</Card.Header>
+                  <Card.Meta>Age: {person.age}</Card.Meta>
+                  <Card.Meta>Background: {person.background}</Card.Meta>
+                  <Card.Description>{person.description}</Card.Description>
+                </Card.Content>
+                <Card.Content extra style={{ textAlign: "center" }}>
+                  <Button
+                    color="red"
+                    onClick={() => {
+                      axios
+                        // .delete("http://localhost/api/v1" + "/person/" + person._id)
+                        .delete("/api/v1/person/" + person._id)
+                        .then((res) => {
+                          console.log(res);
+                          this.fetchPeople();
+                        })
+                        .catch((err) => console.log(err));
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </Card.Content>
+              </Card>
+            );
+          })
+        }
+        </Card.Group>
+      );
+    } else {
+      return(
+          <Card>
+            <Card.Content> There is no data to be fetched </Card.Content>
+        </Card>
+      );
+    }
   };
 
   render() {
